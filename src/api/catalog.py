075e9c12795ebc036a5cf97d99a39eeee0186a92
KallosP <97070073+KallosP@users.noreply.github.com>
2024-04-06 15:38:19 -0700

    added database.py and import statements in endpoint definition files

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+import sqlalchemy
+from src import database as db
 
 router = APIRouter()
 
@@ -9,6 +11,10 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
 
+    # Open connection to DB 
+    #with db.engine.begin() as connection:
+    #    result = connection.execute(sqlalchemy.text(sql_to_execute))
+
     return [
             {
                 "sku": "RED_POTION_0",
@@ -17,4 +23,12 @@ def get_catalog():
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
             }
+            #{
+            #    "sku": "GREEN_POTION_0",
+            #    "name": "green potion",
+            #    "quantity": 1,
+            #    "price": 50,
+            #    # Color green
+            #    "potion_type": [0, 100, 0, 0],
+            #}
         ]
