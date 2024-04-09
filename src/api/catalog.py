834ec64ba6ab1d6346d973ee7d1ce2059ea9446d
KallosP <97070073+KallosP@users.noreply.github.com>
2024-04-08 21:14:08 -0700

    only return a catalog if have stock

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,27 +19,32 @@ def get_catalog():
         rows = result.fetchall()
         # Store the row corresponding to the green potion 
         greenPotionRow = rows[0]
+        numGreenPotions = greenPotionRow[1] 
 
-        return [
-                #{
-                #    "sku": "RED_POTION_0",
-                #    "name": "red potion",
-                #    "quantity": 1,
-                #    "price": 50,
-                #    "potion_type": [100, 0, 0, 0],
-                #}
-                {
+        if numGreenPotions > 0:
 
-                    # FIXME: correct sku?
-                    "sku": "GREEN_POTION_0",
-                    "name": "green potion",
-                    # Dynamically set the quantity based on current value in table (on Supabase)
-                    # Note: temporarily hardcoded to 1 to meet requirements for version 1, change back later to greenPotionRow[1]
-                    "quantity": 1, #greenPotionRow[1],
-                    # Amt I'm selling a green potion for
-                    "price": 50,
-                    # Color green. AKA selling green potions
-                    "potion_type": [0, 100, 0, 0],
-                }
-        ]
+            return [
+                    #{
+                    #    "sku": "RED_POTION_0",
+                    #    "name": "red potion",
+                    #    "quantity": 1,
+                    #    "price": 50,
+                    #    "potion_type": [100, 0, 0, 0],
+                    #}
+                    {
+
+                        # FIXME: correct sku?
+                        "sku": "GREEN_POTION_0",
+                        "name": "green potion",
+                        # Dynamically set the quantity based on current value in table (on Supabase)
+                        # Note: temporarily hardcoded to 1 to meet requirements for version 1, change back later to greenPotionRow[1]
+                        "quantity": 1, #greenPotionRow[1],
+                        # Amt I'm selling a green potion for
+                        "price": 50,
+                        # Color green. AKA selling green potions
+                        "potion_type": [0, 100, 0, 0],
+                    }
+            ]
+    # Don't advertise anything if you have nothing
+    return []
         
